dda815586523655171a5623518e662e08956ea91
Anushree Nema <anushreenema624@gmail.com>
2025-07-09 12:05:22 +0530

    fs module

--- first.js
+++ first.js
@@ -1,1 +1,7 @@
 console.log("coding is nice");
+const fs = require("fs");
+
+fs.writeFile("output.txt", "writing file", (err) => {
+  if (err) console.log("error occured");
+  else console.log("file written successfully");
+});
